e1a14e22a48021076709106ea881a3eb6cd4d5f1
Elton Silva <eltoncarsilva73@gmail.com>
2021-07-06 10:12:18 -0300

    enemy improviments

--- source/entities/actors/Enemy.hx
+++ source/entities/actors/Enemy.hx
@@ -20,6 +20,7 @@ class Enemy extends FlxSprite
 		offset = FlxPoint.get(28, 18);
 		setSize(10, 30);
 		this.setAnimation();
+		this.animation.play("idle");
 	}
 
 	public var stage:PlayState;
@@ -33,8 +34,6 @@ class Enemy extends FlxSprite
 	{
 		if (this.stage.canUpdate() && canAttack())
 		{
-			super.update(elapsed);
-			FlxG.collide(this, this.stage.floor);
 			FlxVelocity.moveTowardsObject(this, this.stage.Player, 20);
 			velocity.set(velocity.x, 200);
 
@@ -47,6 +46,9 @@ class Enemy extends FlxSprite
 			this.animation.play("walk");
 
 		this.flipX = velocity.x > 0 ? false : true;
+
+		super.update(elapsed);
+		FlxG.collide(this, this.stage.floor);
 	}
 
 	public function demage(pointX:Float = 0, pointY:Float = 0)
@@ -63,6 +65,7 @@ class Enemy extends FlxSprite
 
 	public function bleed(pointX:Float = 0, pointY:Float = 0)
 	{
+		this.color = FlxColor.RED;
 		var emitter = new FlxEmitter(20, 20);
 		emitter.setPosition(pointX, pointY);
 
@@ -93,7 +96,7 @@ class Enemy extends FlxSprite
 
 	public function setAnimation():Void
 	{
-		this.animation.add("idle", [0, 1, 2, 3], 5);
+		this.animation.add("idle", [0, 1, 2, 3], 10);
 		this.animation.add("walk", [4, 5, 6, 7], 10);
 	}
 }
